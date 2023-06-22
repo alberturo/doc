6412951aeed0c66e5df8bf75028e1f697572c8f2
Albert Ramirez <alberturo@gmail.com>
2023-06-22 00:15:36 -0500

    ex22 update

--- Algorithms/ex22-toCamelCase.js
+++ Algorithms/ex22-toCamelCase.js
@@ -1,13 +1,27 @@
-//
+//option 1
 
 function toCamelCase(str) {
-  let result = str[0].toUpperCase();
+  let result = "";
   for (letter in str) {
-    if (str[letter - 1] === "") {
+    if (str[letter - 1] === " " || parseInt(letter) === 0) {
       result += str[letter].toUpperCase();
+    } else {
+      result += str[letter];
     }
   }
   return result;
 }
 
 console.log(toCamelCase("hola mundo"));
+
+// option 2
+
+function toCamelCase2(str) {
+  let words = [];
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.substring(1));
+  }
+  return words.join(" ");
+}
+
+console.log(toCamelCase2("hola mundo 2"));
