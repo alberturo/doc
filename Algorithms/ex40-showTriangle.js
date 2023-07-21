9c48ddfbb4814376cd411661f6ded39c753849af
Albert Ramirez <alberturo@gmail.com>
2023-07-21 00:15:24 -0400

    ex40b

--- Algorithms/ex40-showTriangle.js
+++ Algorithms/ex40-showTriangle.js
@@ -1,12 +1,20 @@
 // given a number show the triangle of asterisks * with that number of rows
 
-function showTriangle(row) {
-  for (let i = 1; i <= row; i++) {
+function showTriangle(num) {
+  let middle = Math.floor(num - 1);
+  let result = "";
+  for (let row = 0; row < num; row++) {
     let level = "";
-    for (let j = 1; j <= row; j++) {
-      level += "*";
+    for (let column = 0; column < 2 * num - 1; column++) {
+      if (middle - row <= column && middle + row >= column) {
+        level += "*";
+      } else {
+        level += " ";
+      }
     }
+    result += level + "\n";
   }
+  return result;
 }
 
-console.log(showTriangle(4));
+console.log(showTriangle(14));
