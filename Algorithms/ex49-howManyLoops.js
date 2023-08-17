93c3eb7f598be90f41954698b5434c20efcc1261
Albert Ramirez <alberturo@gmail.com>
2023-08-16 23:55:20 -0500

    ex49

--- Algorithms/ex49-howManyLoops.js
+++ Algorithms/ex49-howManyLoops.js
@@ -1,5 +1,18 @@
-// given a number, return how many loops
+// given a number, return how many loops.
+// the number has a loop when it has a close circule.
 
-function getLoopCount() {}
+function getLoopCount(number) {
+  let arrayNumbers = number.toString().split("");
+  let loops = 0;
+  for (num of arrayNumbers) {
+    num = parseInt(num);
+    if (num === 0 || num === 6 || num === 9) {
+      loops++;
+    } else if (num === 8) {
+      loops += 2;
+    }
+  }
+  return loops;
+}
 
-console.log(getLoopCount);
+console.log(getLoopCount(1998650));
